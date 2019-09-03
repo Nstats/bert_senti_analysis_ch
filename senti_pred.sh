940854a0dd6d1c7c1b08116317525dcd74cd8185
Nstats <17801238715@163.com>
2019-09-03 16:19:43 +0800

    09.03

--- senti_pred.sh
+++ senti_pred.sh
@@ -7,7 +7,8 @@ python senti_analysis.py \
   --output_dir='./data/sentiment/output_wwm_ext' \
   --test_checkpoint_path='./data/sentiment/output_wwm_ext/model.ckpt-6553' \
   --bert_config_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/bert_config.json' \
-  --vocab_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/vocab.txt'; \
+  --vocab_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_L-12_H-768_A-12/bert_model.ckpt'; \
 
 python senti_analysis.py \
   --do_predict=True \
@@ -17,7 +18,8 @@ python senti_analysis.py \
   --output_dir='./data/sentiment/output_webqa' \
   --test_checkpoint_path='./data/sentiment/output_webqa/model.ckpt-6553' \
   --bert_config_file='./data/pretrained_model/bert_ch_webqa/bert_config.json' \
-  --vocab_file='./data/pretrained_model/bert_ch_webqa/vocab.txt'; \
+  --vocab_file='./data/pretrained_model/bert_ch_webqa/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_L-12_H-768_A-12/bert_model.ckpt'; \
 
 python senti_analysis.py \
   --do_predict=True \
@@ -27,7 +29,8 @@ python senti_analysis.py \
   --output_dir='./data/sentiment/output_rmrb' \
   --test_checkpoint_path='./data/sentiment/output_rmrb/model.ckpt-6553' \
   --bert_config_file='./data/pretrained_model/bert_ch_rmrb/bert_config.json' \
-  --vocab_file='./data/pretrained_model/bert_ch_rmrb/vocab.txt'; \
+  --vocab_file='./data/pretrained_model/bert_ch_rmrb/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_L-12_H-768_A-12/bert_model.ckpt'; \
 
 python senti_analysis.py \
   --do_predict=True \
@@ -37,7 +40,8 @@ python senti_analysis.py \
   --output_dir='./data/sentiment/output_bert_original_balanced' \
   --test_checkpoint_path='./data/sentiment/output_bert_original_balanced/model.ckpt-9781' \
   --bert_config_file='./data/pretrained_model/chinese_L-12_H-768_A-12/bert_config.json' \
-  --vocab_file='./data/pretrained_model/chinese_L-12_H-768_A-12/vocab.txt'; \
+  --vocab_file='./data/pretrained_model/chinese_L-12_H-768_A-12/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_L-12_H-768_A-12/bert_model.ckpt'; \
 
 python senti_analysis.py \
   --do_predict=True \
@@ -47,7 +51,8 @@ python senti_analysis.py \
   --output_dir='./data/sentiment/output_wwm_ext_balanced' \
   --test_checkpoint_path='./data/sentiment/output_wwm_ext_balanced/model.ckpt-9781' \
   --bert_config_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/bert_config.json' \
-  --vocab_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/vocab.txt'; \
+  --vocab_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_L-12_H-768_A-12/bert_model.ckpt'; \
 
 python senti_analysis.py \
   --do_predict=True \
@@ -57,7 +62,8 @@ python senti_analysis.py \
   --output_dir='./data/sentiment/output_rmrb_balanced' \
   --test_checkpoint_path='./data/sentiment/output_rmrb_balanced/model.ckpt-9781' \
   --bert_config_file='./data/pretrained_model/bert_ch_rmrb/bert_config.json' \
-  --vocab_file='./data/pretrained_model/bert_ch_rmrb/vocab.txt'; \
+  --vocab_file='./data/pretrained_model/bert_ch_rmrb/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_L-12_H-768_A-12/bert_model.ckpt'; \
 
 python senti_analysis.py \
   --do_predict=True \
@@ -67,7 +73,8 @@ python senti_analysis.py \
   --output_dir='./data/sentiment/output_webqa_balanced' \
   --test_checkpoint_path='./data/sentiment/output_webqa_balanced/model.ckpt-9781' \
   --bert_config_file='./data/pretrained_model/bert_ch_webqa/bert_config.json' \
-  --vocab_file='./data/pretrained_model/bert_ch_webqa/vocab.txt'
+  --vocab_file='./data/pretrained_model/bert_ch_webqa/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_L-12_H-768_A-12/bert_model.ckpt'
 
 :<<annotation
 annotation
