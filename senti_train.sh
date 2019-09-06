867a62740166af459eaae59ae0843fc5cdc5e139
Nstats <17801238715@163.com>
2019-09-06 22:16:40 +0800

    09.06

--- senti_train.sh
+++ senti_train.sh
@@ -9,15 +9,36 @@ python senti_analysis.py \
   --save_checkpoints_steps=1000 \
   --keep_checkpoint_max=1 \
   --save_summary_steps=100 \
-  --classifier='MLP' \
-  --rnn_layers=1 \
-  --output_dir='./data/sentiment/output_RoBERTa_MLP' \
-  --bert_config_file='./data/pretrained_model/roberta_zh_L-24_H-768_A-12/bert_config.json' \
-  --vocab_file='./data/pretrained_model/roberta_zh_L-24_H-768_A-12/vocab.txt' \
-  --init_checkpoint='./data/pretrained_model/roberta_zh_L-24_H-768_A-12/bert_model.ckpt' \
+  --classifier='BiGRU' \
+  --rnn_layers=2 \
+  --output_dir='./data/sentiment/output_wwm_ext_2lBiGru' \
+  --bert_config_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/bert_config.json' \
+  --vocab_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/bert_model.ckpt' \
   --train_dir='train_mix_balanced.tsv' \
   --test_dir='test_mix.tsv' \
-  --test_checkpoint_path='./data/sentiment/output_RoBERTa_MLP/model.ckpt-18258'; \
+  --test_checkpoint_path='./data/sentiment/output_wwm_ext_2lBiGru/model.ckpt-18258'; \
+
+python senti_analysis.py \
+  --do_train=True \
+  --do_predict=True \
+  --max_seq_length=300 \
+  --train_batch_size=24 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=40.0 \
+  --save_checkpoints_steps=1000 \
+  --keep_checkpoint_max=1 \
+  --save_summary_steps=100 \
+  --classifier='BiGRU+highway' \
+  --rnn_layers=1 \
+  --output_dir='./data/sentiment/output_wwm_ext_BiGru_highway' \
+  --bert_config_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/bert_config.json' \
+  --vocab_file='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/vocab.txt' \
+  --init_checkpoint='./data/pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/bert_model.ckpt' \
+  --train_dir='train_mix_balanced.tsv' \
+  --test_dir='test_mix.tsv' \
+  --test_checkpoint_path='./data/sentiment/output_wwm_ext_BiGru_highway/model.ckpt-18258'; \
+
 :<<annotation
 python senti_analysis.py \
   --do_train=True \
